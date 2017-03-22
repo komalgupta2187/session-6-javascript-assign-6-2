219bff9445d59f50e78958aaba666a465a144998
komal Arya <guptakomal87@gmail.com>
2017-03-22 12:07:00 +0530

    session-6-javascript-assign-6-2

---
+++ app.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+number1=20;
+number2=10;
+number3=50;
+
+if (number1>number2 && number1>number3)
+{
+console.log("number1 is greater");
+}
+if (number2>number3 && number2>number1)
+{
+console.log("number2 is greater");
+}
+if (number3>number2 && number3>number1)
+{
+console.log("number3 is greater");
+}
